4e7aa9ced35db88194ec97599d0afe105211b59e
Anton Melnikov <101597291+Melnikov-A@users.noreply.github.com>
2023-11-14 16:51:56 +0300

    Disabled internet Services test

--- src/test/java/beeline/tests/ServicesTest.java
+++ src/test/java/beeline/tests/ServicesTest.java
@@ -1,6 +1,7 @@
 package beeline.tests;
 
 import io.qameta.allure.*;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
@@ -51,6 +52,7 @@ public class ServicesTest extends TestBase {
     }
 
     @ParameterizedTest
+    @Disabled
     @DisplayName("Тест заполнения форм на подключение домашнего интернета")
     @Story("Проверка заполнения форм подключения домашнего интернета с успешной территориальной возможностью подключения")
     @Severity(SeverityLevel.BLOCKER)
